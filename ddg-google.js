32553a39d8adf3676ed9522b1b94050d107fdfd1
chairmanbrando <chairmanbrando@users.noreply.github.com>
2024-12-19 01:20:09 -0500

    Update ddg-google.js

--- ddg-google.js
+++ ddg-google.js
@@ -3,10 +3,10 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://duckduckgo.com/*
 // @grant       none
-// @version     1.8
+// @version     1.9
 // @author      chairmanbrando
-// @description Adds a clickable link to Google in case you forget your `!g`. Typing a "g" without anything having keyboard focus will also send you there!
-// @todo        `event.keyCode` has been deprecated because of reasons. Finally, you can use the 1-9 keys to go to the respective search results while still on DDG.
+// @description Adds a clickable link to Google in case you forget your `!g`. Typing a "g" without anything having keyboard focus will also send you there! Finally, you can use the 1-9 keys to go to the respective search results while still on DDG.
+// @todo        `event.keyCode` has been deprecated because of reasons.
 // @noframes
 // ==/UserScript==
 
@@ -22,8 +22,14 @@ document.body.addEventListener('keyup', (e) => {
   }
 
   // The links also don't seem to exist on `DOMContentLoaded`. 🤨
-  const links = document.body.querySelectorAll('ol li:has(article)');
-  const link  = links[e.keyCode - 49].querySelector('h2 a[href]');
+  //
+  // Also, we need to filter them by those that are visible. I got confused when
+  // "1" tried to send me to a uBO-blocked URL. Turns out there was an ad link
+  // in the first slot that it had hidden for me. Oops.
+  let links = document.body.querySelectorAll('ol li:has(article)');
+      links = Array.from(links).filter((l) => l.checkVisibility());
+
+  const link = links[e.keyCode - 49].querySelector('h2 a[href]');
 
   if (link && link.href) {
     window.location.href = link.href;
@@ -31,8 +37,8 @@ document.body.addEventListener('keyup', (e) => {
 });
 
 const $menu = document.querySelector('#react-duckbar ul:first-of-type'),
-      $li   = $menu.querySelector('li:last-child').cloneNode(true),
-      $a    = $li.querySelector('a');
+        $li = $menu.querySelector('li:last-child').cloneNode(true),
+         $a = $li.querySelector('a');
 
 $a.setAttribute('href', google);
 $a.textContent = 'Google It!';
