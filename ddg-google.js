857f321cfadf0d33f184b5f63887aa98a7dedf94
chairmanbrando <chairmanbrando@users.noreply.github.com>
2024-12-27 21:11:46 -0500

    Update ddg-google.js

--- ddg-google.js
+++ ddg-google.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://duckduckgo.com/*
 // @grant       none
-// @version     1.9.1
+// @version     1.9.2
 // @author      chairmanbrando
 // @description Adds a clickable link to Google in case you forget your `!g`. Typing a "g" without anything having keyboard focus will also send you there! Finally, you can use the 1-9 keys to go to the respective search results while still on DDG.
 // @noframes
@@ -27,17 +27,21 @@ document.body.addEventListener('keyup', (e) => {
   // in the first slot that it had hidden for me. Oops.
   let links = document.body.querySelectorAll('ol li:has(article)');
       links = Array.from(links).filter((l) => l.checkVisibility());
+  let link  = links[e.key.charCodeAt(0) - 49];
 
-  const link = links[e.key.charCodeAt(0) - 49].querySelector('h2 a[href]');
+  if (link) {
+    link.classList.add('going-to-there');
+    link = link.querySelector('h2 a[href]');
 
-  if (link && link.href) {
-    window.location.href = link.href;
+    if (link && link.href) {
+      window.location.href = link.href;
+    }
   }
 });
 
-const $menu = document.querySelector('#react-duckbar ul:first-of-type'),
-        $li = $menu.querySelector('li:last-child').cloneNode(true),
-         $a = $li.querySelector('a');
+const $menu = document.querySelector('#react-duckbar ul:first-of-type');
+const $li   = $menu.querySelector('li:last-child').cloneNode(true);
+const $a    = $li.querySelector('a');
 
 $a.setAttribute('href', google);
 $a.textContent = 'Google It!';
