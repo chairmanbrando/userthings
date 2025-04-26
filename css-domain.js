6baec852dad192bdc9574aab96b3211c23509109
chairmanbrando <chairmanbrando@users.noreply.github.com>
2025-04-26 13:10:01 -0400

    Update css-domain.js

--- css-domain.js
+++ css-domain.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*/*
 // @grant       none
-// @version     1.4
+// @version     1.5
 // @author      chairmanbrando
 // @run-at      document-start
 // @description Runs as soon as possible for a web extension to add the page's domain as a class on the `<html>` element.
@@ -12,31 +12,42 @@
 let hn = window.location.hostname.split('.');
 let sr = null; // Used for reddit only.
 
-while (hn.length > 2) {
-    hn.shift();
+const limit = (looksLikeCountryTld()) ? 3 : 2;
+
+while (hn.length > limit) {
+  hn.shift();
 }
 
 hn = hn.join('-');
 
 if (hn === '0-1') {
-    hn = 'localhost';
+  hn = 'localhost';
 }
 
+// Get the subreddit's name too if relevant.
 if (hn === 'reddit-com') {
-    sr = window.location.pathname.toLowerCase();
-    sr = sr.split('/').filter((p => p.length));
+  sr = window.location.pathname.toLowerCase();
+  sr = sr.split('/').filter((p => p.length));
 
-    if (sr.length > 1) {
-        sr = `${sr[0]}-slash-${sr[1]}`;
-    }
+  if (sr.length > 1) {
+    sr = `${sr[0]}-slash-${sr[1]}`;
+  }
 }
 
+// Do the thing!
 function addClassToDamnedRootElement() {
-    document.documentElement.classList.add(hn);
+  document.documentElement.classList.add(hn);
 
-    if (sr) {
-        document.documentElement.classList.add(sr);
-    }
+  if (sr) {
+    document.documentElement.classList.add(sr);
+  }
+}
+
+// Regex source: https://redd.it/hzcyp9
+function looksLikeCountryTld() {
+  const conts = /(\.|^)(ad|ae|af|ag|ai|al|am|ao|ap|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bl|bm|bn|bo|bq|br|bs|bt|bw|by|bz|ca|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cw|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gt|gu|gw|gy|hk|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mf|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|si|sk|sl|sm|sn|so|sr|ss|st|sv|sx|sy|sz|tc|td|tg|th|tj|tk|tl|tm|tn|to|tr|tt|tv|tw|tz|ua|ug|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)$/;
+
+  return window.location.hostname.match(conts);
 }
 
 // Add our handy class as soon as possible...
@@ -44,17 +55,18 @@ addClassToDamnedRootElement();
 
 //...but also watch for changes and reapply if necessary. These JS frameworks are out of control!
 const observer = new MutationObserver((mutationsList, observer) => {
-    for (const mutation of mutationsList) {
-        if (mutation.attributeName === 'class') {
-            if (!document.documentElement.classList.contains(hn)) {
-                addClassToDamnedRootElement();
-            }
-        }
+  for (const mutation of mutationsList) {
+    if (mutation.attributeName === 'class') {
+      if (! document.documentElement.classList.contains(hn)) {
+        addClassToDamnedRootElement();
+      }
     }
+  }
 });
 
 observer.observe(document.documentElement, {
-    attributes: true,
-    childList: false,
-    subtree: false
+  attributes: true,
+  childList: false,
+  subtree: false
 });
+
