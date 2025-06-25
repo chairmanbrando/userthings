bfc09000c7f02c4ccfca9661cd750d02d2af9bb3
chairmanbrando <chairmanbrando@users.noreply.github.com>
2025-06-25 18:27:59 -0400

    Update bluesky-account-date.js

--- bluesky-account-date.js
+++ bluesky-account-date.js
@@ -4,50 +4,52 @@
 // @match       https://bsky.app/*
 // @grant       none
 // @run-at      document-idle
-// @version     1.1
+// @version     1.2
 // @author      chairmanbrando
-// @description Attempts to add the profile's creation date to their page.
+// @description Attempts to add the profile's creation date to their page. Note that Bluesky says somewhere in its docs that this may not be accurate due to the distributed nature of the network protocol, but I'm sure this is only the case for very few accounts.
 // ==/UserScript==
 
-const endpoint  = 'https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=';
-  let lastTitle = null;
+function addSomethingToStats(something, after) {
+  const link = profile.querySelector(':scope a[href$="/followers"]');
 
-function addCreationDate(list, date) {
-  const item = list.lastChild.cloneNode(true);
+  if (! link) return;
 
-  item.innerHTML = item.innerHTML.replace(/.+?\s/, `${date} `);
-  item.querySelector(':scope > span').textContent = 'born';
+  const list = link.parentElement;
+  const item = list.querySelector(':scope > div').cloneNode(true);
 
+  item.innerHTML = item.innerHTML.replace(/.+?\s/, `${something} `);
+  item.querySelector(':scope > span').textContent = after;
   list.append(item);
 }
 
-// Maybe more stuff will happen here, but for now it's just adding the listed
-// creation date. Note that Bluesky says somewhere in its docs that this may not
-// be accurate due to the distributed nature of the network protocol, but I'm
-// sure this is only the case for very few accounts.
 function addStuffToProfile(data, profile) {
-  if (! data.createdAt) return;
-
   const created = new Date(data.createdAt);
+  const now     = new Date;
 
-  const date = created.toLocaleDateString('en-US', {
+  const since = created.toLocaleDateString('en-US', {
     year:  'numeric',
     month: 'short',
     day:   'numeric'
   }); // "Oct 9, 2024"
 
-  const link = profile.querySelector(':scope a[href$="/followers"]');
+  // Try to minimize weirdness in date difference calculation.
+  created.setHours(12, 0, 0);
+  now.setHours(12, 0, 0);
 
-  if (link) {
-    addCreationDate(link.parentElement, date);
-  }
+  const days = Math.round((now - created) / (1000 * 24 * 60 * 60));
+  const ppd  = (data.postsCount / days).toFixed(2);
+
+  addSomethingToStats(since, 'born');
+  addSomethingToStats(ppd, 'ppd');
 }
 
 function fetchProfileData(username, profile) {
-  fetch(endpoint + username)
+  fetch('https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=' + username)
     .then((resp) => resp.json())
     .then((data) => addStuffToProfile(data, profile));
 }
+
+let lastTitle = null;
 
 const titleWatch = new MutationObserver((mutations, observer) => {
   if (document.title === lastTitle)    return;
@@ -56,11 +58,11 @@ const titleWatch = new MutationObserver((mutations, observer) => {
   username  = document.title.match(/\(@(\S+)\)/).pop();
   lastTitle = document.title;
 
+  const profiles = document.querySelectorAll('div[data-testid="profileScreen"]');
+
   // Bluesky loads and unloads views by some unknown heuristic, hiding the ones
   // that aren't currently being viewed, so more than one profile can exist in
   // the DOM at any time. Thus, we check for the visible one.
-  const profiles = document.querySelectorAll('div[data-testid="profileScreen"]');
-
   for (profile of profiles) {
     if (profile && profile.checkVisibility()) {
       fetchProfileData(username, profile);
@@ -69,5 +71,6 @@ const titleWatch = new MutationObserver((mutations, observer) => {
   }
 });
 
-// It's easier to watch for title changes than piecemeal sections of the DOM.
+// It's easier to watch for title changes than piecemeal sections of the DOM
+// that are filled up asynchronously as various requests come back.
 titleWatch.observe(document.querySelector('title'), { childList: true });
