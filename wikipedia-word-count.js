2567c8b5dfcccf24d61a094a6bfca1013512cbc8
chairmanbrando <chairmanbrando@users.noreply.github.com>
2025-07-02 13:51:50 -0400

    Create wikipedia-word-count.js

---
+++ wikipedia-word-count.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name        Wikipedia: Article Word Count
+// @namespace   Violentmonkey Scripts
+// @match       https://en.wikipedia.org/wiki/*
+// @grant       none
+// @version     1.0
+// @author      chairmanbrando
+// @description Attempts to turn the body content of an article into a string to figure out how many words it contains.
+// ==/UserScript==
+
+// I don't know yet if this works on any other skins than the one I'm using.
+const content = document.querySelector('#bodyContent .mw-parser-output');
+  let cleaned = '';
+
+// Skip many things that would pad the reported length. Invisible things are
+// skipped while certain sections "should" always appear at the bottom, making
+// them a good stopping point. Is this enough? Too much? We'll see.
+for (element of content.querySelectorAll(':scope > *')) {
+  if (! element.checkVisibility())               continue;
+  if (element.querySelector('#See_also'))        break;
+  if (element.querySelector('#References'))      break;
+  if (element.querySelector('#Further_reading')) break;
+
+  cleaned += element.textContent.trim();
+}
+
+// More cleanup for increased accuracy.
+cleaned = cleaned.replaceAll('[edit source]', ' ');
+cleaned = cleaned.replaceAll(/\[\d+?\]/gm, ' ');
+cleaned = cleaned.replaceAll(/\s+?/gm, ' ');
+
+// I couldn't get reruns of the last pattern above to work for whatever reason.
+while (cleaned.includes('  ')) {
+  cleaned = cleaned.replaceAll('  ', ' ');
+}
+
+cleaned = cleaned.trim();
+cleaned = cleaned.split(' ').length;
+
+if (! cleaned) return;
+
+const lnavu = document.querySelector('#left-navigation ul');
+const litem = `<li class="mw-list-item words">~${cleaned} words</li>`;
+const style = '<style>.mw-list-item.words { margin: 0 8px; padding: 12px 0 7px }</style>';
+
+lnavu.insertAdjacentHTML('beforeend', litem + style);
