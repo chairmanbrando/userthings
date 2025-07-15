f798855646ea20dc6d863f667750382b005f1c87
chairmanbrando <chairmanbrando@users.noreply.github.com>
2025-07-15 01:55:19 -0400

    Update ddg-google.js

--- ddg-google.js
+++ ddg-google.js
@@ -3,7 +3,8 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://duckduckgo.com/*
 // @grant       none
-// @version     1.10.0
+// @run-at      document-idle
+// @version     1.10.1
 // @author      chairmanbrando
 // @description Adds a clickable link to Google in case you forget your `!g`. Typing a "g" without anything having keyboard focus will also send you there! Finally, you can use the 1-9 keys to go to the respective search results while still on DDG.
 // @require     https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
