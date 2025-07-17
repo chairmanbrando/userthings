c7605ffedfc94d50452aafeaae018d77fff55f76
chairmanbrando <chairmanbrando@users.noreply.github.com>
2025-07-16 23:55:04 -0400

    Update css-domain.js

--- css-domain.js
+++ css-domain.js
@@ -3,19 +3,24 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*/*
 // @grant       none
-// @version     1.5
+// @version     1.6.1
 // @author      chairmanbrando
 // @run-at      document-start
 // @description Runs as soon as possible for a web extension to add the page's domain as a class on the `<html>` element.
 // ==/UserScript==
 
 let hn = window.location.hostname.split('.');
+let su = null; // One subdomain allowed.
 let sr = null; // Used for reddit only.
 
-const limit = (looksLikeCountryTld()) ? 3 : 2;
+const limit = 3; // (looksLikeCountryTld()) ? 3 : 3;
 
 while (hn.length > limit) {
   hn.shift();
+}
+
+if (hn.length === 3) {
+  su = hn.shift();
 }
 
 hn = hn.join('-');
@@ -36,6 +41,10 @@ if (hn === 'reddit-com') {
 
 // Do the thing!
 function addClassToDamnedRootElement() {
+  if (su && su !== 'www') {
+    document.documentElement.classList.add(su);
+  }
+
   document.documentElement.classList.add(hn);
 
   if (sr) {
@@ -43,7 +52,7 @@ function addClassToDamnedRootElement() {
   }
 }
 
-// Regex source: https://redd.it/hzcyp9
+// @deprecated Regex source: https://redd.it/hzcyp9
 function looksLikeCountryTld() {
   const conts = /(\.|^)(ad|ae|af|ag|ai|al|am|ao|ap|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bl|bm|bn|bo|bq|br|bs|bt|bw|by|bz|ca|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cw|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gt|gu|gw|gy|hk|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mf|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|si|sk|sl|sm|sn|so|sr|ss|st|sv|sx|sy|sz|tc|td|tg|th|tj|tk|tl|tm|tn|to|tr|tt|tv|tw|tz|ua|ug|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)$/;
 
@@ -69,4 +78,3 @@ observer.observe(document.documentElement, {
   childList: false,
   subtree: false
 });
-
