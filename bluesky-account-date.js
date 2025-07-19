5a719c89ab9afbb9e7d2d6a86714b1da4bc9a75a
chairmanbrando <chairmanbrando@users.noreply.github.com>
2025-07-19 04:15:34 -0400

    Update bluesky-account-date.js

--- bluesky-account-date.js
+++ bluesky-account-date.js
@@ -4,25 +4,23 @@
 // @match       https://bsky.app/*
 // @grant       none
 // @run-at      document-idle
-// @version     1.3.1
+// @version     1.4.0
 // @author      chairmanbrando
 // @description Attempts to add the creation date and a posts-per-day average to a user's profile. Note that Bluesky says somewhere in its docs that this may not be accurate due to the distributed nature of the network protocol, but I'm sure this is only the case for very few accounts.
+// @require     https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
+// @noframes
 // ==/UserScript==
-
-// Got tired of passing this div around function to function. Most didn't even
-// use it; they simply passed it on untouched. That was a smell, and so is this.
-let visibleProfile = null;
-let lastUsername   = null;
 
 function addThingToStats(thing, after, list) {
   const item = list.querySelector(':scope > div').cloneNode(true);
 
+  item.classList.add('stat', `stat-${after}`);
   item.innerHTML = item.innerHTML.replace(/.+?\s/, `${thing} `);
   item.querySelector(':scope > span').textContent = after;
   list.append(item);
 }
 
-function addStuffToProfile(data) {
+function addStuffToProfile(data, profile) {
   const created = new Date(data.createdAt);
   const now     = new Date;
 
@@ -43,53 +41,38 @@ function addStuffToProfile(data) {
   // ulates the profile's line of stats. I didn't much feel like doing nested
   // MutationObservers at the time I was debugging this issue, so I cheated.
   statsWatch = setInterval(() => {
-    let link = visibleProfile.querySelector(':scope a[data-testid="profileHeaderFollowersButton"]');
+    let link = profile.querySelector(':scope a[data-testid="profileHeaderFollowersButton"]');
 
     if (link) {
+      clearInterval(statsWatch);
       addThingToStats(since, 'born', link.parentElement);
       addThingToStats(ppd, 'spd', link.parentElement);
-      clearInterval(statsWatch);
+
+      link.parentElement.querySelectorAll(':scope > :is(div, a)').forEach((li) => li.classList.add('stat'));
     }
   }, 100);
 }
 
 function fetchProfileData(username) {
-  return fetch('https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=' + username)
-    .then((resp) => resp.json());
+  const endpoint = 'app.bsky.actor.getProfile?actor=';
+  console.log('BACDA fetching:', endpoint + username);
+
+  return fetch(`https://public.api.bsky.app/xrpc/${endpoint + username}`).then((resp) => resp.json());
 }
 
-const titleWatch = new MutationObserver((mutations, observer) => {
-  if (document.title.includes('did:')) return;
-  if (! document.title.includes('@'))  return;
+// It's more efficient to look within a container, but the one that contains
+// profiles shows up asynchronously too and things go awry. `arrive()` is a nicer
+// syntax for basic MutationObserver stuff, and if that class is inefficient then
+// I guess this will be too.
+//
+// @@ Doesn't always fire when loading your own profile from the sidebar.
+document.arrive('div[data-testid="profileScreen"]', (profile) => {
+  let username = document.title.match(/@([\w\.-]+)/).pop();
+      username = username.replace(')', ''); // Caught by `\S+`.
 
-  // Profile document titles are formatted in one of the following ways:
-  //
-  // - Name (@username) — Bluesky
-  // - @username — Bluesky
-  //
-  // We have to account for people who don't set a dedicated display name.
-  let username = document.title.match(/\(?@(\S+)\)?/).pop();
-      username = username.replace(')', '');
-
-  // Sometimes the title changes twice at discrete enough times, one without the
-  // display name and then another with it, that our callback here is run twice.
-  // We have to stop execution in that case or we'll end up with dupes.
-  if (username === lastUsername) return;
-
-  lastUsername = username;
-
-  // Bluesky loads and unloads views by some unknown heuristic, hiding the ones
-  // that aren't currently being viewed, and this means more than one profile
-  // can exist in the DOM at the same time. Thus, we check for the visible one.
-  for (const profile of document.querySelectorAll('div[data-testid="profileScreen"]')) {
-    if (profile && profile.checkVisibility()) {
-      visibleProfile = profile;
-      fetchProfileData(username).then(addStuffToProfile);
-      break;
-    }
+  if (username) {
+    fetchProfileData(username).then((data) => {
+      addStuffToProfile(data, profile);
+    });
   }
 });
-
-// It's easier to watch for title changes than piecemeal sections of the DOM
-// that are filled up asynchronously as various requests come back.
-titleWatch.observe(document.querySelector('title'), { childList: true });
