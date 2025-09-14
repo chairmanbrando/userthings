dfd9821b1ee46ab2c705087df729df7243e0ea03
chairmanbrando <chairmanbrando@users.noreply.github.com>
2025-09-13 23:02:37 -0400

    Update twitter-favicon.js

--- twitter-favicon.js
+++ twitter-favicon.js
@@ -3,10 +3,11 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://x.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      chairmanbrando
 // @description Replaces the stupid 𝕏 favicon, which is nigh unrecognizable in a
-//              list of tabs, with the old blue bird guy.
+//              list of tabs, with the old blue bird guy. Oh, and now it also 
+//              changes "X" to "Twitter" in tab titles.
 // ==/UserScript==
 
 let favicon = document.querySelector('link[rel="shortcut icon"]');
@@ -14,3 +15,15 @@ let favicon = document.querySelector('link[rel="shortcut icon"]');
 if (favicon) {
   favicon.href = 'https://abs.twimg.com/favicons/twitter.ico';
 }
+
+setInterval(() => {
+  let title = document.querySelector('title');
+
+  if (title && title.textContent.includes('X')) {
+    let reXTitle = / \/ X$/;
+
+    if (reXTitle.test(title.textContent)) {
+      title.textContent = title.textContent.replace(reXTitle, ' / Twitter');
+    }
+  }
+}, 250);
