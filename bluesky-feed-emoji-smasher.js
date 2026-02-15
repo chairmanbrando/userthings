e1dfe5523f1bf21613e3e687e2fcc277da42bf86
chairmanbrando <chairmanbrando@users.noreply.github.com>
2026-02-14 22:16:57 -0500

    Update bluesky-feed-emoji-smasher.js
    Seems they made the feed list more performant recently.

--- bluesky-feed-emoji-smasher.js
+++ bluesky-feed-emoji-smasher.js
@@ -3,33 +3,37 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://bsky.app/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      chairmanbrando
-// @description Does what it says, yo!
+// @description Emojis in feed names always bothered me, but Bluesky recently added
+//              separate icons to them, so now those feeds are doubled up. Now it's
+//              bothering me doubly, so the emojis must die.
 // ==/UserScript==
 
 // @note `{Emoji}` is too broad and hits numbers. If this one isn't enough, one
 // even further beyond exists: `{Extended_Pictographic}`
-const emojiRegex  = /\p{Emoji_Presentation}/u;
-const emojiRegexG = /\p{Emoji_Presentation}/gu;
-const justTextDiv = ':scope > div:not(:has(div, img, svg))';
+const emojiRegex = /\p{Emoji_Presentation}/gu;
 
-// Since Bluesky is all asychronous *and* lacks classes and IDs on elements, we
-// have to resort to watching for specific elements amongst the entire soup.
+function emojiCheck(div) {
+  if (div.getAttribute('aria-label')?.match(emojiRegex)) {
+    const label = div.querySelector('div[dir]');
+    label.textContent = label.textContent.replace(emojiRegex, '');
+  }
+}
+
+// Initial check in case they're in the DOM before this script runs.
+document.querySelectorAll('div[aria-label]').forEach(emojiCheck);
+
+// If not, since Bluesky is asychronous *and* lacks classes and IDs on elements,
+// we have to resort to watching for specific elements amongst the entire soup.
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     mutation.addedNodes.forEach((node) => {
       if (node.nodeType === 1) {
-        if (node.tagName === 'DIV' && node.getAttribute('aria-label')?.match(emojiRegex)) {
-          const label = node.querySelector(justTextDiv);
-          label.textContent = label.textContent.replace(emojiRegexG, '');
-        }
+        node.querySelectorAll?.('div[aria-label]').forEach(emojiCheck);
       }
     });
   });
 });
 
-observer.observe(document.body, {
-  childList: true,
-  subtree: true
-});
+observer.observe(document.body, { childList: true, subtree: true });
