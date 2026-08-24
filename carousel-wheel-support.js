9481dd60eed2d9849000a9515bf7e3b18d1ac6f5
chairmanbrando <chairmanbrando@users.noreply.github.com>
2026-08-24 18:44:37 -0400

    Update carousel-wheel-support.js

--- carousel-wheel-support.js
+++ carousel-wheel-support.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name        Cycle Carousels with Wheel
 // @namespace   Violentmonkey Scripts
+// @match       https://apnews.com/*
 // @match       https://bsky.app/*
 // @match       https://www.imdb.com/*
 // @match       https://*.wikipedia.org/wiki/*
 // @grant       none
-// @version     1.1.3
+// @version     1.2.4
 // @author      chairmanbrando
 // @description Add a `@match` directive each time you find yourself on a site
 //              you use a lot and would be better with carousel scrolling. Some
@@ -14,25 +15,32 @@
 //              forward/back with arrow keys, this'll attach the wheel to 'em.
 // ==/UserScript==
 
-function maybeSendArrowKeyInstead(e, selector) {
+function maybeSendArrowKeyInstead(e, selector, prereq) {
   if (! Array.from(document.querySelectorAll(selector)).some(el => el.checkVisibility())) return;
+  if (! Array.from(document.querySelectorAll(prereq)).some(el => el.checkVisibility())) return;
 
   e.preventDefault();
 
-  const key = e.deltaY < 0 ? 'ArrowLeft' : 'ArrowRight';
+  const target  = document.activeElement || document.body;
+  const key     = e.deltaY < 0 ? 'ArrowLeft' : 'ArrowRight';
   const keyCode = key === 'ArrowLeft' ? 37 : 39;
 
-  document.dispatchEvent(new KeyboardEvent('keydown', {
-    key, code: key, keyCode, which: keyCode, bubbles: true
-  }));
+  if (prereq) {
+    prereq = document.querySelector(prereq);
+    prereq.setAttribute('tabindex', '-1');
+    prereq.focus();
+  }
+
+  target.dispatchEvent(new KeyboardEvent('keydown', { key, code: key, keyCode, which: keyCode, bubbles: true }));
 }
 
-function wheelMeBaby(selector) {
-  document.addEventListener('wheel', (e) => maybeSendArrowKeyInstead(e, selector), { passive: false });
+function wheelMeBaby(selector, prereq = null) {
+  document.addEventListener('wheel', (e) => maybeSendArrowKeyInstead(e, selector, prereq), { passive: false });
 }
 
 const domain = new URL(window.location.href).hostname;
 
+if (domain.includes('apnews.com'))    wheelMeBaby('.flickity-button', '.CarouselOverlay-slides');
 if (domain.includes('bsky.app'))      wheelMeBaby('button[aria-label$="image"]');
 if (domain.includes('imdb.com'))      wheelMeBaby('.media-viewer__page-right');
 if (domain.includes('wikipedia.org')) wheelMeBaby('.mw-mmv-button');
